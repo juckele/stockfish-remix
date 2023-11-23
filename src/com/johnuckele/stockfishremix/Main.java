1e9b06527b40feca89cf1cdea2dc1bcabf670407
John H. Uckele <john.h.uckele@gmail.com>
2023-11-23 09:20:16 -0500

    Add default stockfish path
    TODO: use and provide an ability to override

--- src/com/johnuckele/stockfishremix/Main.java
+++ src/com/johnuckele/stockfishremix/Main.java
@@ -2,6 +2,8 @@ package com.johnuckele.stockfishremix;
 
 public class Main {
 
+    private static final String DEFAULT_STOCKFISH_PATH = "/home/juckele/stockfish/stockfish-ubuntu-x86-64-avx2";
+
     public static void main(String[] args) {
         System.out.println("Stockfish Remix");
         System.out.println("Creating board");
